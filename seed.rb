b86b41494be1ae818c974fc3e88cab7b3e7b4c91
Denis <denisosa911@gmail.com>
2018-03-19 23:48:22 +0200

    Refactored seed.rb

--- seed.rb
+++ seed.rb
@@ -4,11 +4,9 @@ require_relative 'shop'
 require_relative 'products_category'
 apple = Product.new('apple', 7, 6)
 pineapple = Product.new('pineapple', 9, 8)
-products_fruits = [apple, pineapple]
 pork = Product.new('pork', 11, 10)
 beef = Product.new('beef', 15, 14)
-products_meat = [pork, beef]
-fruits = ProductsCategory.new('fruits', products_fruits)
-meat = ProductsCategory.new('meat', products_meat)
+fruits = ProductsCategory.new('fruits', [apple, pineapple])
+meat = ProductsCategory.new('meat', [pork, beef])
 
 @shop = Shop.new('shop', [fruits, meat])
